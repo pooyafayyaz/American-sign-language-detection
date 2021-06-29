79c82e285b84a2d95aadb75654027e8a4dac1c81
pooya fayyaz <fayyazpooya@gmail.com>
2021-06-29 19:07:38 -0400

    pipline done for two videos

--- motion_detection.py
+++ motion_detection.py
@@ -12,71 +12,68 @@ def variance_of_laplacian(image):
 
 
 def get_motion_seq(file):
-        # Read the video file 
-        cap = cv2.VideoCapture(file) 
-
+        # Read the video file
+        cap = cv2.VideoCapture(file)
 
         # Read the first 2 frames
         _,first_frame = cap.read()
         ret,second_frame = cap.read()
 
-        # Set the threshold to remove small movments 
+        # Set the threshold to remove small movments
         MOVEMENT_THRESHOLD= 3000
         i=0
-        start_motion_frame = None 
+        start_motion_frame = None
         last_motion_frame = None
         while (ret) :
-                # Get the difference between two consecutive frames	
+                # Get the difference between two consecutive frames
                 frame_delta = cv2.absdiff(first_frame, second_frame)
-                # Convert it to grayscale video 
+                # Convert it to grayscale video
                 # Contours are easier to find in grayscale mode
                 gray = cv2.cvtColor(frame_delta, cv2.COLOR_BGR2GRAY)
-                # Blur the frame 
+                # Blur the frame
                 blur = cv2.GaussianBlur(gray, (5, 5), 0)
-                
-                # Set the threshold 
+
+                # Set the threshold
                 thresh = cv2.threshold(blur, 25, 255, cv2.THRESH_BINARY)[1]
-                
+
                 # Fill in holes
                 dilate = cv2.dilate(thresh, None, iterations = 3)
-                
+
                 # Find the contours, ignore the hierarchy
                 cnts, _ = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-                
-                
+
+
                 # loop over the contours
                 for c in cnts:
 
                         # Coordinates
                         (x, y, w, h) = cv2.boundingRect(c)
-                        
+
                         # If the contour is too small, ignore it, otherwise, there's transient
                         # movement
                         if cv2.contourArea(c) > MOVEMENT_THRESHOLD:
                             # Set the frames for motion
-                            if start_motion_frame ==None:
-                                start_motion_frame = i 
+                            if start_motion_frame == None:
+                                start_motion_frame = i
                             if last_motion_frame == None or last_motion_frame < i:
                                 last_motion_frame = i
-                            
+
                             # Draw a rectangle
                             cv2.rectangle(first_frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
                 frame_delta = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
-                
+
                 #cv2.imshow("img", np.hstack((frame_delta, first_frame)))
                 #cv2.imwrite("frame"+ str(i) + ".png", np.hstack((frame_delta, first_frame)))
                 first_frame = second_frame
                 ret,second_frame = cap.read()
                 i+=1
                 #cv2.waitKey(0)
-                
-        print(start_motion_frame,last_motion_frame)
+
+        #print(start_motion_frame,last_motion_frame)
         # Close the video
-        cv2.waitKey(0)
+        #cv2.waitKey(0)
         cv2.destroyAllWindows()
         cap.release()
 
         return start_motion_frame,last_motion_frame
-
-
--- whole_pipline.py
+++ whole_pipline.py
@@ -1,62 +1,88 @@
 from motion_detection import get_motion_seq
 import numpy as np
 from dtaidistance import dtw_ndim
+from dtaidistance import dtw
+import os
+from natsort import natsorted
+from math import sqrt
 
-file1 =
-file2 = 
+file1 = "/Users/pooya/Downloads/result/body-lightbulb_qian_1_rgb/"
+file2 = "/Users/pooya/Downloads/result/body-lightbulb_sofia_14_rgb/"
+
+raw_file1 = "/Users/pooya/Downloads/cropped_bodypatches/body-lightbulb_qian_1_rgb.avi"
+raw_file2 = "/Users/pooya/Downloads/cropped_bodypatches/body-lightbulb_sofia_14_rgb.avi"
+
+
 
 def read_json(file):
     pose_seq = []
+    for r, d, f in os.walk(file):
+        #sort based on the frame number
+        for file in natsorted(f):
+            if file.endswith(".json"):
 
-    with open(file, "r") as read_file:
-        data = read_file.read()
-        data = data.replace('array', 'np.array').replace("float32","np.float32")
-        d = eval(data)
-        # all the key points
-        body_Pose = d[0]["keypoints"]
+                with open(r+file, "r") as read_file:
+                    data = read_file.read()
+                    data = data.replace('array', 'np.array').replace("float32","np.float32")
+                    d = eval(data)
+                    # all the key points
+                    body_Pose = d[0]["keypoints"]
 
-        #Normilize the points
-        #Nose position
-        neck_position = (body_Pose[5] + body_Pose[6])/2
-        # Shoulder positions
-        right_shoulder = body_Pose[6]
-        left_shoulder = body_Pose[5]
+                    #Normilize the points
+                    #Nose position
+                    neck_position = (body_Pose[5] + body_Pose[6])/2
+                    # Shoulder positions
+                    right_shoulder = body_Pose[6]
+                    left_shoulder = body_Pose[5]
 
-        #Calculate the distance between two shoulders
-        dist = sqrt( (left_shoulder[0] - right_shoulder[0])**2 + (left_shoulder[1] - right_shoulder[1])**2 )
+                    #Calculate the distance between two shoulders
+                    dist = sqrt( (left_shoulder[0] - right_shoulder[0])**2 + (left_shoulder[1] - right_shoulder[1])**2 )
 
-        # 1. First we have to set the origin to neck position
-        # 2. Scale all the coordinates so that the distance of right
-        # shoulder to left shoulder is one
+                    # 1. First we have to set the origin to neck position
+                    # 2. Scale all the coordinates so that the distance of right
+                    # shoulder to left shoulder is one
 
-        right_center = np.sum(body_Pose[91:112,:], axis=0)/21
-        left_center =  np.sum(body_Pose[112:133,:], axis=0)/21
+                    right_center = np.sum(body_Pose[91:112,:], axis=0)/21
+                    left_center =  np.sum(body_Pose[112:133,:], axis=0)/21
 
-        body_Pose[5:11,0] = (body_Pose[5:11,0] - neck_position[0])/dist
-        body_Pose[5:11,1] = (body_Pose[5:11,1] - neck_position[1])/dist
+                    body_Pose[5:11,0] = (body_Pose[5:11,0] - neck_position[0])/dist
+                    body_Pose[5:11,1] = (body_Pose[5:11,1] - neck_position[1])/dist
 
-        body_Pose[91:112,0] = (body_Pose[91:112,0] - body_Pose[91][0])
-        body_Pose[91:112,1] = (body_Pose[91:112,1] - body_Pose[91][1])
+                    body_Pose[91:112,0] = (body_Pose[91:112,0] - body_Pose[91][0])
+                    body_Pose[91:112,1] = (body_Pose[91:112,1] - body_Pose[91][1])
 
-        body_Pose[112:133,0] = (body_Pose[112:133,0] - body_Pose[112][0])
-        body_Pose[112:133,1] = (body_Pose[112:133,1] - body_Pose[112][1])
+                    body_Pose[112:133,0] = (body_Pose[112:133,0] - body_Pose[112][0])
+                    body_Pose[112:133,1] = (body_Pose[112:133,1] - body_Pose[112][1])
 
-        # Using only body and hand pose data, all other key points will be discarded
-        body_Pose = body_Pose[:,0:2]
+                    # Using only body and hand pose data, all other key points will be discarded
+                    body_Pose = body_Pose[:,0:2]
 
-        body_Pose = body_Pose[np.r_[5:11, 91:133], :]
+                    body_Pose = body_Pose[np.r_[5:11, 91:133], :]
 
-        # Otherwise just use the body points
-        #body_Pose = body_Pose[np.r_[5:11], :]
+                    # Otherwise just use the body points
+                    #body_Pose = body_Pose[np.r_[5:11], :]
 
-        pose_seq.append(body_Pose)
+                    pose_seq.append(body_Pose)
 
-        return pose_seq
+    return np.array(pose_seq)
 
 pose_seq1 = read_json(file1)
 pose_seq2 = read_json(file2)
 
-start_vid1 ,end_vid1 = get_motion_seq(file1)
-start_vid2 ,end_vid2 = get_motion_seq(file2)
+print(pose_seq1.shape)
+print(pose_seq2.shape)
 
-_ , dtw_matrix =  dtw_ndim.distance(pose_seq1.astype(np.double), pose_seq2.astype(np.double))
+start_vid1 ,end_vid1 = get_motion_seq(raw_file1 )
+start_vid2 ,end_vid2 = get_motion_seq(raw_file2 )
+start_vid ,end_vid = max(start_vid1,start_vid2),max(end_vid1,end_vid2)
+
+print(start_vid1 ,end_vid1)
+print(start_vid2 ,end_vid2)
+print(start_vid ,end_vid)
+
+_ , dtw_matrix =  dtw_ndim.warping_paths(pose_seq1.astype(np.double), pose_seq2.astype(np.double))
+dtw_matrix  = dtw_matrix[1:,1:]
+best_path = dtw.best_path(dtw_matrix)
+best_path = np.array(best_path)
+print(best_path)
+print(best_path[ ((start_vid<best_path) & (best_path< end_vid)).all(axis=1) ])
