4b29a838e626e3edb4f72f4e02af3150af6f65ab
Pooya <pfayyazs@gmu.edu>
2021-07-08 19:50:30 -0400

    dataloader

--- Contrastive_Learning/dataloader.py
+++ Contrastive_Learning/dataloader.py
@@ -7,14 +7,23 @@ import torch
 
 
 class HandPatchdataset(Dataset):
+
+
+    def row_to_array(data_str):
+    	 return np.array(data_str[1:-1].strip().split()).reshape((-1,2))    	 
+    	
     def __init__(self,data_path,sample_rate_sk):
         self.data = pd.read_csv(data_path)
-
+        self.data.iloc[:,2] = self.data.iloc[:,2].apply(row_to_array)
+        self.dataset = [] 
+        for index, row in data.iterrows():
+       	for item in row[2]:
+       		self.dataset.add([row[0],row[1],item])
+       		
     def __len__(self):
-        return self.data["video"].max()
+        return self.dataset.shape[0]
 
     def __getitem__(self,idx):
-        
-        return (torch.Tensor(sampled_frames),torch.Tensor(label))
+        return []
 
 
